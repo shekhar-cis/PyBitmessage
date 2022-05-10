aff6ca1bd7acc4d1013fd6c65c4130888f6f92cf
shekhar-cis <shekhar.c@cisinlabs.com>
2022-05-10 15:53:06 +0530

    mpybit[update draft kivy state variable]&add delte inside helper_sent

--- src/helper_sent.py
+++ src/helper_sent.py
@@ -7,3 +7,7 @@ from helper_sql import sqlExecute
 def insert(t):
     """Perform an insert into the `sent` table"""
     sqlExecute('''INSERT INTO sent VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', *t)
+
+def delete(ack_data):
+    """Perform delete ack data"""
+    sqlExecute("DELETE FROM sent WHERE ackdata = ?", ack_data)
